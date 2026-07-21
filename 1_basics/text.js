8441f18d183476d50317d9160f663522d0bcd009
RahulKarthik34 <rahulkarthik017@gmail.com>
2026-07-21 06:10:35 +0000

    container commit done

--- 1_basics/text.js
+++ 1_basics/text.js
@@ -1,3 +1,8 @@
-console.log("hello rahul")
-console.log("hello rahul")
-console.log("hello rahul")
+const userID = 12312
+let userEmail = "rahul@gmail.com"
+var userPassword ="12312"
+userCiry ="nellore"
+userEmail="karthik@gmail.com"
+
+
+console.table([userID,userEmail,userPassword,userCiry])
