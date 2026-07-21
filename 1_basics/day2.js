69b40451f5276e0225b531dd91157ca767831111
RahulKarthik34 <rahulkarthik017@gmail.com>
2026-07-21 06:30:33 +0000

    container commit done

---
+++ 1_basics/day2.js
@@ -0,0 +1,32 @@
+//conversion 
+
+let score = 0
+
+//console.log(typeof(score));
+
+
+let value = Number(score)
+
+
+//console.log(typeof(value));
+
+console.log(value);
+
+let isLoggedIn = ""
+
+//console.log(isLoggedIn )
+
+let boolIslogedIn =  Boolean(isLoggedIn)
+
+//console.log(boolIslogedIn)
+
+// 1=> true ;  0 =>  false ;empty "" => false ; any string " rahul" => true ;
+
+
+let stringNumber = 33
+
+
+
+let someNUmber =  String(stringNumber)
+
+console.log(someNUmber)
