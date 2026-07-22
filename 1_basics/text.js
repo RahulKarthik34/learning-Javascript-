983d66f04222885b734eaf8b9d37272c94383add
RahulKarthik34 <rahulkarthik017@gmail.com>
2026-07-22 19:06:24 +0530

    Delete 1_basics directory

--- 1_basics/text.js
+++
@@ -1,8 +0,0 @@
-const userID = 12312
-let userEmail = "rahul@gmail.com"
-var userPassword ="12312"
-userCiry ="nellore"
-userEmail="karthik@gmail.com"
-
-
-console.table([userID,userEmail,userPassword,userCiry])
