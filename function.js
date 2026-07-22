8c932a70d5abdb09c51e22fb1d1a76638ecac47a
Rahul <rahulkarthik017@gmail.com>
2026-07-22 19:07:18 +0530

    Day 1: Added HTML, CSS, and JavaScript files

---
+++ function.js
@@ -0,0 +1,19 @@
+
+
+const main1= "welcome to the javascript"
+
+let userName = "karthik"
+let taskcount = 0 
+
+document.getElementById("nameOfuser").textContent=
+
+` welcome  ${userName}`
+
+document.getElementById("title").textContent=
+
+`${main1}!`
+
+document.getElementById("taskCount") .textContent =
+`${taskcount}`
+
+
