9e71ac7723b21294de394614d52aa4fdee63882e
Rahul <rahulkarthik017@gmail.com>
2026-07-22 23:28:49 +0530

    day 1 completed

---
+++ day11/function.js
@@ -0,0 +1,28 @@
+
+
+// const main1= "welcome to the javascript"
+
+// let userName = "karthik"
+// let taskcount = 0 
+
+// document.getElementById("nameOfuser").textContent=
+
+// ` welcome  ${userName}`
+
+// document.getElementById("title").textContent=
+
+// `${main1}!`
+
+// document.getElementById("taskCount") .textContent =
+// `${taskcount}`
+
+
+//  prompt is like an user input 
+
+let userName = prompt("enter your name")
+let userCity =prompt("enyer you city name ")
+
+alert(`welcome ${userName}`)
+console.log("name:"+userName)
+console.log("city:"+userCity)
+
